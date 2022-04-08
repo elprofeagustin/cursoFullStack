b7da9889ab3c34f2ba056e5741e14d10b6aa64fc
elprofeagustin <ammedinagus2@gmail.com>
2022-04-07 22:59:55 -0300

    clase dia jueves 7/04/22

---
+++ js/funciones_login.js
@@ -0,0 +1,7 @@
+function dame_datos()
+{
+    let u = document.getElementById('login').value;
+    let p = document.getElementById('password').value;
+    alert(u);
+    alert(p);
+}
--- js/misfunciones.js
+++ js/misfunciones.js
@@ -1,7 +1,32 @@
-function sumar(n,p)
+function gestion_password()
 {
-   return n + p;
+   let v1 = prompt("ingrese contraseña entre 8 y 10 caracteres");
+   let r=v1.length;
+   if(r>=8 && r<=10)
+   {
+      alert("parametros correctos <br>");
+      alert("pass: " + v1.charAt(0) + "...." + v1.charAt(r-1))
+   }
+   else
+   {
+      alert("error: la contraseña no cumple los requisitos <br>");
+   }
 }
 
+function dame_palabra(n)
+{
+   let mipalabra = n.substring(0,5);
+   alert(mipalabra);
+   let r = n.indexOf("@");
+   if(r != -1)
+   {
+      alert("datos correctos");
+   }
+   else
+   {
+      alert("incorrecto" )
+   }
+   alert(n.toUpperCase());
+   alert(n.toLowerCase());
 
-document.write(sumar(5,10));
+}
