ab465c2a4952012ec60947b21d73c12f6e626c40
elprofeagustin <ammedinagus2@gmail.com>
2022-04-21 23:00:31 -0300

    clase jueves 21/04

--- js/misfunciones.js
+++ js/misfunciones.js
@@ -1,42 +1,31 @@
+window.addEventListener('load',inicio);
+function inicio()
+{
+   document.getElementById("pagar").addEventListener('click',generarventa);
+}
 precio_producto=0;
+cant_productos=0;
 misproductos = [];
 function agregarproducto(p,d) {
    precio_producto = precio_producto + p;
    misproductos.push(d);
-   alert("Se agrego el articulo para la venta");
+   cant_productos+=1;
+   document.getElementById("c1").value=cant_productos;
  }
-
-function gestion_password()
+function quitararticulo(p)
 {
-   let v1 = prompt("ingrese contraseña entre 8 y 10 caracteres");
-   let r=v1.length;
-   if(r>=8 && r<=10)
+   if(cant_productos<=0)
    {
-      alert("parametros correctos <br>");
-      alert("pass: " + v1.charAt(0) + "...." + v1.charAt(r-1))
+      alert("no se puede quitar mas productos")
    }
    else
    {
-      alert("error: la contraseña no cumple los requisitos <br>");
+      cant_productos-=1;
+      document.getElementById("c1").value=cant_productos;
+      misproductos.pop();
+      precio_producto=precio_producto - p;
    }
-}
-
-function dame_palabra(n)
-{
-   let mipalabra = n.substring(0,5);
-   alert(mipalabra);
-   let r = n.indexOf("@");
-   if(r != -1)
-   {
-      alert("datos correctos");
-   }
-   else
-   {
-      alert("incorrecto" )
-   }
-   alert(n.toUpperCase());
-   alert(n.toLowerCase());
-
+      
 }
 function generarventa()
 {
@@ -50,7 +39,7 @@ function generarventa()
      cliente: n_c,
      importe: precio_producto,
      fecha: mifecha,
-     producto : misproductos,
+     producto: misproductos,
   };
   console.log("Detalle de la venta");
   console.log("cliente: " + venta.cliente);
@@ -60,27 +49,4 @@ function generarventa()
   {
      console.log(venta.producto[f]);
   }
-}
-
-function juegodearreglos()
-{
-   let misdatos = [];
-   misdatos.push(5);
-   misdatos.push("hola");
-   misdatos.push(505);
-   misdatos.push(24);
-   delete misdatos[1];
-
-   for(let f=0;f<misdatos.length;f++)
-   {
-      console.log(misdatos[f]);
-   }
-   for(let f in misdatos)
-   {
-      console.log(misdatos[f]);
-   }
-   misdatos.forEach(function(v,f)
-   {
-      console.log(v);
-   });
 }
